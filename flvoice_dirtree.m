3f6cb46dbb41ee6316ab2dfcb0390e1c580ba575
Alfonso Nieto-Castanon <alfnie@gmail.com>
2023-11-18 01:12:18 +0100

    misc
    faster gui updates

--- flvoice_dirtree.m
+++ flvoice_dirtree.m
@@ -1,8 +1,8 @@
 function varargout=flvoice_dirtree(filepath,varargin)
-% [sub,ses,run] = flvoice_dirtree(filepath)
+% [sub,ses,run,task] = flvoice_dirtree(filepath)
 
 if nargin<1||isempty(filepath), filepath=flvoice('PRIVATE.ROOT'); end
-if flvoice('remote'), [varargout{1:nargout}]=conn_server('run',mfilename,conn_server('util_localfile',filepath),varargin{:}); return; end
+%if flvoice('remote'), [varargout{1:nargout}]=conn_server('run',mfilename,conn_server('util_localfile',filepath),varargin{:}); return; end
 varargout=cell(1,nargout);
 
 [nill,SUB]=cellfun(@fileparts,conn_dir(fullfile(filepath,'sub-*'),'-dir','-R','-cell'),'uni',0);
@@ -22,18 +22,25 @@ SUB=SUBS;
 SUBS={};
 SESS=[];
 RUNS={};
+TASKS={};
 for nsample=1:numel(SUB)
     [nill,runs1]=cellfun(@fileparts,conn_dir(fullfile(filepath,sprintf('sub-%s',SUB{nsample}),sprintf('ses-%d',SES(nsample)),'beh',sprintf('sub-%s_ses-%d_run-*_desc-audio.mat',SUB{nsample},SES(nsample))),'-R','-cell'),'uni',0);
     [nill,runs2]=cellfun(@fileparts,conn_dir(fullfile(filepath,sprintf('sub-%s',SUB{nsample}),sprintf('ses-%d',SES(nsample)),'beh',sprintf('sub-%s_ses-%d_run-*.mat',SUB{nsample},SES(nsample))),'-R','-cell'),'uni',0);
-    runs=union(runs1,runs2);
-    runs=unique(regexprep(runs,'^.*_(run-[^\._]*)[\._].*$','$1'));
-    %[nill,runs]=cellfun(@fileparts,conn_dir(fullfile(filepath,sprintf('sub-%s',SUB{nsample}),sprintf('ses-%d',SES(nsample)),'beh','run-*'),'-dir','-R','-cell'),'uni',0);
-    RUNS=[RUNS; runs(:)];
-    SESS=[SESS; repmat(SES(nsample),numel(runs),1)];
-    SUBS=[SUBS; repmat(SUB(nsample),numel(runs),1)];
+    allruns=union(runs1,runs2);
+    if ~isempty(allruns)
+        runs=regexprep(allruns,'^.*_(run-[^\._]*)[\._].*$','$1');
+        tasks=regexprep(allruns,{'^.*_task-','_expParams$|_desc-audio$'},'');
+        [nill,idx]=unique(cellstr([char(runs) char(tasks)]));
+        RUNS=[RUNS; runs(idx)];
+        TASKS=[TASKS; tasks(idx)];
+        SESS=[SESS; repmat(SES(nsample),numel(idx),1)];
+        SUBS=[SUBS; repmat(SUB(nsample),numel(idx),1)];
+    end
 end
 RUN=str2double(regexprep(RUNS,'^run-',''));
+TASK=TASKS;
 SES=SESS;
 SUB=SUBS;
 maskout=isnan(RUN);
-varargout={SUB(~maskout),SES(~maskout),RUN(~maskout)};
+varargout={SUB(~maskout),SES(~maskout),RUN(~maskout),TASK(~maskout)};
+
